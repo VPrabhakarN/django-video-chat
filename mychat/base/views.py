ee24a65e47cf9b4b83ab7e5433e1562d07477876
VPrabhakarN <vijaynagane19@gmail.com>
2026-07-24 19:02:43 +0530

    Successfully configured

--- mychat/base/views.py
+++ mychat/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Lobby Page View
+def lobby(request) :
+    return render(request, 'base/lobby.html')
+
+# Room Page View
+def room(request) :
+    return render(request, 'base/room.html')
+
