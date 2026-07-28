7ae3c6e0ffdd2aa8e738d29a46ef9c276c83fb0c
VPrabhakarN <vijaynagane19@gmail.com>
2026-07-28 09:10:33 +0530

    Button Added

--- mychat/static/js/script.js
+++ mychat/static/js/script.js
@@ -7,7 +7,10 @@ const client = AgoraRTC.createClient({mode : 'rtc', codec : 'vp8'})
 let localTracks = []
 let remoteUsers = {}
 
-let joiAndDisplayLocalSream = async () => {
+let joinAndDisplayLocalSream = async () => {
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
@@ -19,10 +22,58 @@ let joiAndDisplayLocalSream = async () => {
                       <div class="video-player" id="user-${UID}"></div>
                     </div>`
 
-    document.getElementById('video-streams').insertAdjancentHTML('beforeend', player)
+    // Fixed typo: insertAdjancentHTML -> insertAdjacentHTML
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
 
-    localTracks[1].play('user-${UID}')
+    // Fixed interpolation: 'user-${UID}' -> `user-${UID}`
+    localTracks[1].play(`user-${UID}`)
 
     await client.publish([localTracks[0], localTracks[1]])
 }
+
+let handleUserJoined = async(user, mediaType) => {
+    remoteUsers[user.uid] = user 
+    await client.subscribe(user, mediaType)
+
+    if (mediaType === 'video') {
+
+    let player = document.getElementById(`user-container-${user.uid}`)
+
+    if (player) {
+        player.remove()
+    }
+
+    player = `
+    <div class="video-container" id="user-container-${user.uid}">
+        <div class="username-wrapper">
+            <span class="user-name">My Name</span>
+        </div>
+        <div class="video-player" id="user-${user.uid}"></div>
+    </div>
+    `
+
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    user.videoTrack.play(`user-${user.uid}`)
+}
+
+if (mediaType === 'audio') {
+    user.audioTrack.play()
+}
+}
+
+let handleUserLeft = async(user) => {
+    delete remoteUsers[user.uid]
+
+    let player = document.getElementById(`user-container-${user.uid}`)
+
+    if (player) {
+        player.remove()
+    }
+}
+
+
+
+joinAndDisplayLocalSream()
+
 
